d253a86b4cb8dd56332bf4d00cc350fa01fe08f0
Jacksgong <igzhenjie@gmail.com>
2016-02-04 14:33:30 +0800

    test: add SmoothHandler integration to ProgressBar

--- demo/src/main/java/cn/dreamtobe/percentsmoothhandler/demo/SmoothProgressBar.java
+++ demo/src/main/java/cn/dreamtobe/percentsmoothhandler/demo/SmoothProgressBar.java
@@ -6,10 +6,15 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
 
+import java.lang.ref.WeakReference;
+
+import cn.dreamtobe.percentsmoothhandler.ISmoothTarget;
+import cn.dreamtobe.percentsmoothhandler.SmoothHandler;
+
 /**
  * Created by Jacksgong on 2/4/16.
  */
-public class SmoothProgressBar extends ProgressBar {
+public class SmoothProgressBar extends ProgressBar implements ISmoothTarget {
 
     public SmoothProgressBar(Context context) {
         super(context);
@@ -26,5 +31,26 @@ public class SmoothProgressBar extends ProgressBar {
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public SmoothProgressBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public float getPercent() {
+        return getProgress() / (float) getMax();
+    }
+
+    @Override
+    public void setPercent(float percent) {
+        setProgress((int) Math.ceil(percent * getMax()));
+    }
+
+    private SmoothHandler smoothHandler;
+
+    @Override
+    public void setSmoothPercent(float percent) {
+        if (smoothHandler == null) {
+            smoothHandler = new SmoothHandler(new WeakReference<ISmoothTarget>(this));
+        }
+
+        smoothHandler.loopSmooth(percent);
     }
 }

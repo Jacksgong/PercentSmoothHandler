a5dc48949c78915812c714fb947bbce78c06f8dc
Jacksgong <igzhenjie@gmail.com>
2016-04-21 18:11:46 +0800

    demo: add smoothly processing with a definite duration demo

--- demo/src/main/java/cn/dreamtobe/percentsmoothhandler/demo/MainActivity.java
+++ demo/src/main/java/cn/dreamtobe/percentsmoothhandler/demo/MainActivity.java
@@ -5,9 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatSeekBar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SeekBar;
+
+import cn.dreamtobe.percentsmoothhandler.SmoothHandler;
 
 /**
  * Created by Jacksgong on 2/4/16.
@@ -19,7 +24,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         assignViews();
-        smoothPb.setSmoothPercent(0.5f);
+        smoothPb.setSmoothPercent(0.3f);
+        SmoothHandler.NEED_LOG = true;
+
+        increaseDurationSmoothSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                increaseDurationSmoothBtn.setText(
+                        getString(R.string.increase_duration_definite_smoothly, getDuration()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 
     public void onClickNormalIncrease(final View view) {
@@ -30,8 +52,16 @@ public class MainActivity extends AppCompatActivity {
         smoothPb.setSmoothPercent(getIncreasePercent());
     }
 
+    public void onClickSmoothDurationIncrease(final View view) {
+        smoothPb.setSmoothPercent(getIncreasePercent(), getDuration());
+    }
+
     public void onClickRest(final View view) {
         smoothPb.setPercent(0);
+    }
+
+    private int getDuration() {
+        return increaseDurationSmoothSb.getProgress()/*10ms*/ * 100;
     }
 
     private float getIncreasePercent() {
@@ -62,11 +92,21 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
     private SmoothProgressBar smoothPb;
+    private AppCompatSeekBar increaseDurationSmoothSb;
+    private AppCompatButton increaseDurationSmoothBtn;
+    private AppCompatButton increaseSmoothlyBtn;
+    private AppCompatButton increaseNormalBtn;
+    private AppCompatButton resetBtn;
 
     private void assignViews() {
         smoothPb = (SmoothProgressBar) findViewById(R.id.smooth_pb);
+        increaseDurationSmoothSb = (AppCompatSeekBar) findViewById(R.id.increase_duration_smooth_sb);
+        increaseDurationSmoothBtn = (AppCompatButton) findViewById(R.id.increase_duration_smooth_btn);
+        increaseSmoothlyBtn = (AppCompatButton) findViewById(R.id.increase_smoothly_btn);
+        increaseNormalBtn = (AppCompatButton) findViewById(R.id.increase_normal_btn);
+        resetBtn = (AppCompatButton) findViewById(R.id.reset_btn);
     }
+
 
 }
